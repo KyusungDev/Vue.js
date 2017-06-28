96a8aea2c591fb9866053658f8bff50af0d8d620
paul <paul@novonetworks.com>
2017-06-28 13:23:59 +0900

    작업 내용 추가

---
+++ StockTrader/src/components/portfolio/Stock.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="col-md-4 col-sm-6">
+    <div class="panel panel-info">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{ propStock.name }}
+          <small>(Price: {{ propStock.price }} | Quantity: {{propStock.quantity }})</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input 
+              type="number"
+              class="form-control"
+              placeholder="Quantity"
+              v-model.number="quantity"
+              :class="{danger: insufficientQuantity}"
+          >
+        </div>
+        <div class="pull-right">
+          <button
+              class="btn btn-success"
+              @click="sellStock"
+              :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+              >{{insufficientQuantity ? 'Too Much' : 'Sell'}}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scpoed>
+  .danger,
+  .danger:focus {
+    border: 1px solid red;
+  }
+</style>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    props: ['propStock'],
+    
+    data() {
+      return {
+        quantity: 0
+      }
+    },
+    computed: {
+      funds() {
+        return this.$store.getters.funds;
+      },
+      insufficientQuantity() {
+        return this.quantity > this.propStock.quantity;
+      }
+    },
+    methods: {
+      ...mapActions({
+        placeSellOrder: 'sellStock'
+      }),
+      sellStock() {
+        const order = {
+          stockId: this.propStock.id,
+          stockPrice: this.propStock.price,
+          quantity: this.quantity
+        };
+        this.placeSellOrder(order);
+        this.quantity = 0;
+      }
+    }
+
+  }
+</script>
---
+++ StockTrader/src/components/stocks/Stock.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="col-md-4 col-sm-6">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{ propStock.name }}
+          <small>(Price: {{ propStock.price }} )</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input 
+              type="number"
+              class="form-control"
+              placeholder="Quantity"
+              v-model.number="quantity"
+              :class="{danger: insufficientFunds}"
+          >
+        </div>
+        <div class="pull-right">
+          <button
+              class="btn btn-success"
+              @click="buyStock"
+              :disabled="insufficientFunds || quantity <= 0 || !Number.isInteger(quantity)"
+              >{{ insufficientFunds ? 'Too Much' : 'Buy' }}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .danger,
+  .danger:focus {
+    border: 1px solid red;
+  }
+</style>
+
+<script>
+  export default {
+    props: ['propStock'],
+    
+    data() {
+      return {
+        quantity: 0
+      }
+    },
+    computed: {
+      funds() {
+        return this.$store.getters.funds;
+      },
+      insufficientFunds() {
+        return this.quantity * this.propStock.price > this.funds;
+      }
+    },
+    methods: {
+      buyStock() {
+        const order = {
+          stockId: this.propStock.id,
+          stockPrice: this.propStock.price,
+          quantity: this.quantity,
+        };
+        this.$store.dispatch('buyStock', order);
+        this.quantity = 0;
+      }
+    }
+
+  }
+</script>
